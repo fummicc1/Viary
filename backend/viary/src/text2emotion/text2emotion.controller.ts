aa1b99afc46154009f22397d50e8a0175d578907
Ubuntu <ubuntu@ip-172-26-1-2.ap-northeast-1.compute.internal>
2022-11-23 12:22:40 +0000

    Fix JS: `String.replace` replaces only first match unless use global match of regex.:

--- backend/viary/src/text2emotion/text2emotion.controller.ts
+++ backend/viary/src/text2emotion/text2emotion.controller.ts
@@ -6,7 +6,15 @@ export class Text2emotionController {
     constructor(private readonly text2emotionService: Text2emotionService) { }
     @Get()
     fetch(@Query('text') text: string): string {
-        const res = this.text2emotionService.execute(text);
-        return res;
+        const texts = text.split("\n");
+        let allResult = [];
+        for (const text of texts) {
+            const result = this.text2emotionService.execute(text);
+            allResult.push(result);
+        }
+        const response = {
+            "results": allResult,
+        };
+        return JSON.stringify(response);
     }
 }
