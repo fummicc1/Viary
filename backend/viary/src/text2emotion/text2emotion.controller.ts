1bd3054bca481f2ae891a49456547a7af03ac19b
Ubuntu <ubuntu@ip-172-26-1-2.ap-northeast-1.compute.internal>
2022-11-25 16:59:15 +0000

    Update backend

--- backend/viary/src/text2emotion/text2emotion.controller.ts
+++ backend/viary/src/text2emotion/text2emotion.controller.ts
@@ -6,8 +6,9 @@ export class Text2emotionController {
     constructor(private readonly text2emotionService: Text2emotionService) { }
     @Get()
     fetch(@Query('text') text: string): string {
-        const texts = text.split("\n");
+        const texts = text.split(".");
         let allResult = [];
+        console.log("texts:", texts);
         for (const text of texts) {
             const result = this.text2emotionService.execute(text);
             allResult.push(result);
