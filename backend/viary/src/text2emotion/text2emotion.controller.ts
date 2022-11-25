f9b5de2027597bc5f5079ed8f3e4dda85dd2fdab
Fumiya Tanaka <fumiya.tennis1@gmail.com>
2022-11-26 01:59:52 +0900

    Backend (#3)

--- backend/viary/src/text2emotion/text2emotion.controller.ts
+++ backend/viary/src/text2emotion/text2emotion.controller.ts
@@ -6,7 +6,16 @@ export class Text2emotionController {
     constructor(private readonly text2emotionService: Text2emotionService) { }
     @Get()
     fetch(@Query('text') text: string): string {
-        const res = this.text2emotionService.execute(text);
-        return res;
+        const texts = text.split(".");
+        let allResult = [];
+        console.log("texts:", texts);
+        for (const text of texts) {
+            const result = this.text2emotionService.execute(text);
+            allResult.push(result);
+        }
+        const response = {
+            "results": allResult,
+        };
+        return JSON.stringify(response);
     }
 }
