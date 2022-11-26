56c4e054164c8a8bd41dd355770426705ff59a96
Ubuntu <ubuntu@ip-172-26-1-2.ap-northeast-1.compute.internal>
2022-11-26 15:31:19 +0000

    Add ja2en

---
+++ ml/ja2en.py
@@ -0,0 +1,16 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import argparse
+
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-ja-en")
+
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-ja-en")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("text", default="")
+    text = parser.parse_args().text
+    batch = tokenizer([text], return_tensors="pt")
+    generated_ids = model.generate(**batch)
+    ret = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    print(ret)
