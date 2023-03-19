d8cfb7cc935abda34a26bc640edf9d3006201658
fummicc1 <fumiya.tennis1@gmail.com>
2023-03-19 12:51:47 +0900

    Create EmotionDetector

---
+++ mobile/ios/Features/CreateViaryFeature/Sources/CreateViaryFeature/ML/EmotionDetector.swift
@@ -0,0 +1,15 @@
+//
+//  EmotionDetector.swift
+//  
+//
+//  Created by Fumiya Tanaka on 2023/03/19.
+//
+
+import Foundation
+import CoreML
+
+public protocol EmotionDetector { }
+
+public class EmotionDetectorImpl {}
+
+extension EmotionDetectorImpl: EmotionDetector {}
