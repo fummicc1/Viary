bf50d1366385e315c657c9d9fb717a4b6d30facf
Fumiya Tanaka <fumiya.tennis1@gmail.com>
2023-04-27 00:31:05 +0900

    Create dev and prod config (#43)

---
+++ ios-dev/ios-dev/ios_devApp.swift
@@ -0,0 +1,18 @@
+//
+//  ios_devApp.swift
+//  ios-dev
+//
+//  Created by Fumiya Tanaka on 2023/04/27.
+//
+
+import SwiftUI
+import App
+
+@main
+struct ios_devApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppScreen()
+        }
+    }
+}
---
+++ ios-prod/ios-prod/ios_prodApp.swift
@@ -0,0 +1,18 @@
+//
+//  ios_prodApp.swift
+//  ios-prod
+//
+//  Created by Fumiya Tanaka on 2023/04/27.
+//
+
+import SwiftUI
+import App
+
+@main
+struct ios_prodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppScreen()
+        }
+    }
+}
