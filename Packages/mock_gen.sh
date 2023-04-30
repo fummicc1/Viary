01fed3bc41fe32041c82d4b13a84eaa87a7de9e4
Fumiya Tanaka <fumiya.tennis1@gmail.com>
2023-05-01 01:17:32 +0900

    Update mock (#46)
    * Add new empty package. Update mock
    
    * Fix
    
    * Update

--- Packages/mock_gen.sh
+++ Packages/mock_gen.sh
@@ -1,1 +1,18 @@
-mockolo -s ./Repositories/Sources/Repositories -d ./Repositories/Sources/Repositories/RepositoryMock.swift
+base=$(realpath $(dirname $0))
+
+dir[0]=$base/EmotionDetection/Sources/EmotionDetection
+dir[1]=$base/LocalDataStore/Sources/LocalDataStore
+dir[2]=$base/Repositories/Sources/Repositories
+dir[3]=$base/SpeechToText/Sources/SpeechToText
+
+dir[4]=$base/UseCases/ViaryUseCase/Sources/ViaryUseCase
+
+
+for path in ${dir[@]}; do
+path=$(realpath $path)
+if [ ! -d $path ];then
+    continue;
+fi
+echo "$path"
+mockolo -s $path -d $path/Mock.swift
+done
