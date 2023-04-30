a07c06ebbe9ab3d46afbeeac7e142f8d3195251e
fummicc1 <fumiya.tennis1@gmail.com>
2023-05-01 01:05:12 +0900

    Add new empty package. Update mock

--- Packages/mock_gen.sh
+++ Packages/mock_gen.sh
@@ -1,1 +1,11 @@
-mockolo -s ./Repositories/Sources/Repositories -d ./Repositories/Sources/Repositories/RepositoryMock.swift
+dir=$(realpath $(dirname $0))
+echo $dir
+
+for path in $(ls $dir); do
+path=$(realpath $path)
+if [ ! -d $path ];then
+    continue;
+fi
+echo "$path"
+mockolo -s $path -d $path/Mock.swift
+done
