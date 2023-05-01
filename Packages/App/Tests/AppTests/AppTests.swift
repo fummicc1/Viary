5ece6c2a21ab8f11ec3921f5475cbca9c3a62ae9
fummicc1 <fumiya.tennis1@gmail.com>
2023-05-01 11:10:44 +0900

    Update build workflow and add TestPlan

--- Packages/App/Tests/AppTests/AppTests.swift
+++ Packages/App/Tests/AppTests/AppTests.swift
@@ -1,11 +1,4 @@
 import XCTest
 @testable import App
 
-final class AppTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(App().text, "Hello, World!")
-    }
-}
+final class AppTests: XCTestCase {}
