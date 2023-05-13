7a858ad75ee02b7752c00ec06e0831c0d1fdf746
Fumiya Tanaka <fumiya.tennis1@gmail.com>
2023-05-13 13:09:17 +0900

    Call huggingface api and modify tests (#63)

--- Packages/Features/EditViaryFeature/Tests/EditViaryFeatureTests/ModeTests.swift
+++ Packages/Features/EditViaryFeature/Tests/EditViaryFeatureTests/ModeTests.swift
@@ -31,10 +31,17 @@ final class ModeTests: XCTestCase {
         let repository = ViaryRepositoryMock()
         let baseViary = Viary(
             id: Tagged<Viary, String>(uuid.uuidString),
-            messages: [],
+            messages: [
+                Viary.Message(
+                    viaryID: Tagged<Viary, String>(uuid.uuidString),
+                    id: Tagged<Viary.Message, String>(uuid.uuidString),
+                    sentence: "Hello",
+                    lang: .en
+                )
+            ],
             date: date
         )
-        myViaries = .init([])
+        myViaries = .init([baseViary])
         repository.myViaries = myViaries.eraseToAnyPublisher()
         let store = TestStore(
             initialState: EditViary.State(
@@ -48,6 +55,29 @@ final class ModeTests: XCTestCase {
             })
         )
         XCTAssertEqual(store.state.mode, EditViary.Mode.view)
+        await store.send(
+            .editMessageSentence(
+                id: baseViary.messages.first!.id,
+                sentence: "Hello!!"
+            )
+        )
+        await store.send(.toggleMode) {
+            $0.mode = .edit
+        }
+        await store.send(
+            .editMessageSentence(
+                id: baseViary.messages.first!.id,
+                sentence: "Hello!!"
+            )
+        ) {
+            let id: Tagged<Viary.Message, String> = Tagged(uuid.uuidString)
+            $0.editable.messages[id: id]?.sentence = "Hello!!"
+            $0.resolved[id] = false
+        }
+    }
 
+    override func tearDown() {
+        super.tearDown()
+        myViaries.send(completion: .finished)
     }
 }
---
+++ Playgrounds/Ja2EnServiceSandBox/Ja2EnServiceSandBox/Ja2EnServiceSandBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ja2EnServiceSandBoxApp.swift
+//  Ja2EnServiceSandBox
+//
+//  Created by Fumiya Tanaka on 2023/05/13.
+//
+
+import SwiftUI
+
+@main
+struct Ja2EnServiceSandBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
