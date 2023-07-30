b2c35dc1b7b1ba8fc80a367419e6ed9101fa5055
fummicc1 <fumiya.tennis1@gmail.com>
2023-07-30 11:13:33 +0900

    fix: CI and Tests

--- Tests/Ja2EnTests/Ja2EnTests.swift
+++ Tests/Ja2EnTests/Ja2EnTests.swift
@@ -3,9 +3,5 @@ import XCTest
 
 final class Ja2EnTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Ja2En().text, "Hello, World!")
     }
 }
